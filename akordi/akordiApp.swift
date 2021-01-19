f6aced3cbb0b718bb901afdef198c8c591b0c2b0
Desislav Hristov <hristov.desislav@gmail.com>
2021-01-20 01:02:44 +0300

    Initial Commit

---
+++ akordi/akordiApp.swift
@@ -0,0 +1,17 @@
+//
+//  akordiApp.swift
+//  akordi
+//
+//  Created by Desislav Hristov on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct akordiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
